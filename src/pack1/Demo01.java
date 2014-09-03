ca7a57084a6455b0855c6a206994f11192455dd3
ci.research.mah@gmail.com <ci.research.mah@gmail.com@e352e791-3135-2c7f-1b96-d44206ddbcaa>
2014-09-03 19:35:49 +0000

    cleaned project and updated build.xml, also added Demo02.java

--- src/pack1/Demo01.java
+++ src/pack1/Demo01.java
@@ -2,19 +2,27 @@ package pack1;
 
 public class Demo01 {
 	
+	public void run() {
+		System.out.println("--running Demo01--");
+		demoMethod1();
+	}
+	
 	private void demoMethod1() {
 		int foo = 5;
 		int bar = 10;
 		
 		int res = foo+bar;
-		System.out.print("res: "+res);
+		System.out.println("res: "+res);
 	}
 
 	public static void main(String[] args){
-		Demo01 demo = new Demo01();
-		demo.demoMethod1();
+		Demo01 demo01 = new Demo01();
+		demo01.run();
 		// new change...
 		// another change...
 		// yet another change...
+		
+		Demo02 demo02 = new Demo02();
+		demo02.run();
 	}
 }
---
+++ src/pack1/Demo02.java
@@ -0,0 +1,17 @@
+package pack1;
+
+public class Demo02 {
+
+	public void run() {
+		System.out.println("\n--running Demo02--");
+		demoMethod1();
+	}
+
+	private void demoMethod1() {
+		System.out.println("Demo02 isn't doing anything..");
+	}
+	
+//	public static void main(String[] args){
+//		System.out.println("evil main");
+//	}
+}
